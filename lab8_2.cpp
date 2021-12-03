d01d7443569696286c3cb294d9efb0dff16e98e5
Fluke <hunterx52110@gmail.com>
2021-12-03 15:26:35 +0700

    lab8_2completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -16,3 +16,42 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+#include <iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+    string name,movie,time,st,sp = ": ";
+    char en  = '\n';
+    int gear,yearnum;
+
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?"<< en <<"?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! "<< name<<" is a really cool name."<<en<<"Fahsai: I think you are an Engineering student. What is your student ID?"<<en<<name<<sp;
+    cin >> gear;
+    cin.ignore();
+    
+    if(gear>100000000){
+    gear = gear*pow(10,-7);
+    yearnum = gear-12;
+    }
+
+    cout << "Fahsai: I think you may be GEAR " << yearnum << ". I have a free movie ticket for you."<< en << "Fahsai: Let's go to the cinema together!!!" <<en;
+    cout<<"Fahsai: What movie do you want to watch?"<<en <<name<< sp;
+    getline(cin,movie);
+
+    cout << "Fahsai: So....which day are you free to go with me?" << en << name << sp;
+    getline(cin,time);
+
+    cout << "Fahsai: "<< time << "....that is OK!!! I'm looking forward to watching " << movie<<" with you."<<en <<name<<sp;
+    cin.ignore();
+
+    cout<< "Fahsai: 555+ see you " << time << ". Bye Bye \\(^ ^)/";
+
+        
+        
+
+
+    }
